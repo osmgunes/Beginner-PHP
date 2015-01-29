c9209c599ea9516006e5db4ba0febe3ff7d3f102
osman gunes <osm.gunes@hotmail.com>
2015-01-30 00:45:38 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,21 +1,40 @@
+
 <?php
+echo str_word_count("Hello world!");
 $txt = "Hello world!";
+echo strrev($txt);
+echo strpos($txt,"world");
 $x = 5;
 $y = 10.5;
 
-echo $txt;
-echo "\n";
-echo $x;
-echo "\n";
-echo $y;
-   echo $x+$y;
-function osm()
+function osm($d1,$d2)
 {
-$a=5;
-$b=3;
-echo $a+$b;
- 
-    
+return $d1*$d2;
 }
-osm();
+echo osm($x,$y);
+ var_dump($x);
+ $human = array("osm","brn","mrt");
+foreach($human as $yaz)
+{
+    
+    echo $yaz;
+}
+echo "\nsası..:". count($human);
+if($x!=$y)
+{
+    echo "\nx y ye esit degil\n";
+}
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+arsort($age);
+foreach($age as $k => $k_value) {
+echo "Key=" . $k . ", Value=" . $k_value;
+echo "\n";
+
+}//echo $_SERVER['PHP_SELF'];
+//echo $_SERVER['SERVER_NAME'];
+//echo $_SERVER['HTTP_HOST'];
+//echo $_SERVER['HTTP_REFERER'];
+//echo $_SERVER['HTTP_USER_AGENT'];
+//echo $_SERVER['SCRIPT_NAME'];
+//  http://www.w3schools.com/php/php_superglobals.asp
 ?>
